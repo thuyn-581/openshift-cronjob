fa9e209e017b308a2da19421ace02f0ac96f20cb
Thuy Nguyen <thnguyen@redhat.com>
2024-07-02 14:43:24 -0400

    debug ocp auth

--- app.py
+++ app.py
@@ -13,18 +13,17 @@ from kubernetes.client import ApiClient
 
 
 def authenticate():
-    config.load_incluster_config()
     # Check if code is running in OpenShift
-    # if "acm-cronjob" in os.environ['HOSTNAME']:
-    #     config.load_incluster_config()
-    #     file_token = open(
-    #         "/run/secrets/kubernetes.io/serviceaccount/token", "r"
-    #     )
-    #     if file_token.mode == "r":
-    #         token = file_token.read()
-    #         print("token: %s\n" %(token))
-    # else:
-    #     config.load_kube_config()    
+    if os.getenv('KUBERNETES_SERVICE_HOST'):
+        config.load_incluster_config()
+        file_token = open(
+            "/run/secrets/kubernetes.io/serviceaccount/token", "r"
+        )
+        if file_token.mode == "r":
+            token = file_token.read()
+            print("token: %s\n" %(token))
+    else:
+        config.load_kube_config()    
 
     # Create a client config
     k8s_client = config.new_client_from_config()
